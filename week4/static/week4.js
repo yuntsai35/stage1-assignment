82028dd7ee4e9933f43a8a384e62fd451656b99f
yuntsai35 <amytsai563@gmail.com>
2025-11-08 15:46:05 +0800

    Add files via upload

---
+++ week4/static/week4.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  var checkBox = document.getElementById("checkbox");
+
+  if (checkBox.checked == false){
+    
+    alert("請勾選同意條款");
+  }else{
+    document.getElementById("login").submit();
+  }
+}
+
+function positivenumber(){
+  var number=document.getElementById("hotel_id");
+  var id=number.value.trim()
+  var int_id=Number(id)
+
+  if (!Number.isInteger(int_id) || int_id<=0){
+    alert("請輸入正整數")
+  }else {
+   window.location.href = "/hotel/" + int_id;
+}}
